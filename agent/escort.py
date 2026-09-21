581990b8947fac63a284aa65a9da3040676e1721
melancholyFishAndWater <2633193509@qq.com>
2026-09-21 15:16:15 +0800

    fix: 修复防卫局任务只接取一次就结束的问题，顺便优化了移动逻辑

---
+++ agent/escort.py
@@ -0,0 +1,40 @@
+from typing import Any, Literal
+
+from maa.agent.agent_server import AgentServer
+from maa.custom_recognition import CustomRecognition
+from maa.custom_action import CustomAction
+from maa.context import (
+    Context,
+    JRecognitionType,
+    JActionType,
+    Rect,
+    TemplateMatchResult,
+    RecognitionDetail,
+    OCRResult,
+)
+from maa.pipeline import JTemplateMatch, JOCR, JSwipe
+from re import search
+from numpy import ndarray, dtype
+
+_count = 0
+
+
+# 检测防卫局任务是否结束 目前是检测到十次后返回非 None
+@AgentServer.custom_recognition("CheckEscortEndReco")
+class CheckEscortEndReco(CustomRecognition):
+    def analyze(
+        self, context: Context, argv: CustomRecognition.AnalyzeArg
+    ) -> (
+        CustomRecognition.AnalyzeResult
+        | Rect
+        | list[int]
+        | ndarray[tuple[Any, ...], dtype[Any]]
+        | tuple[int, int, int, int]
+        | None
+    ):
+        global _count
+        if _count < 10:
+            _count += 1
+        else:
+            _count = 0
+            return []
