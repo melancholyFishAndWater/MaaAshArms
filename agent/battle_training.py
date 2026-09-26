4deecad6b58c0717b9a35caaac2fe07937a8a999
melancholyFishAndWater <2633193509@qq.com>
2026-09-27 01:56:56 +0800

    feat: 添加训练所相关功能
    - 添加面板上选择关卡难度的功能
    - 优化了任务逻辑
    - 添加注释说明

--- agent/battle_training.py
+++ agent/battle_training.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any
 from unittest import result
 
@@ -17,9 +18,12 @@ from re import search
 
 from base import addListToTuple, toTuple
 
+# ---------- Reco ----------
 
-@AgentServer.custom_recognition("EnterTrainingTeamRepo")
-class EnterTrainingTeamRepo(CustomRecognition):
+
+# 识别关卡名 返回还有挑战次数的关卡名字box
+@AgentServer.custom_recognition("TrainingEnterOneRepo")
+class TrainingEnterOneRepo(CustomRecognition):
     def analyze(
         self, context: Context, argv: CustomRecognition.AnalyzeArg
     ) -> (
@@ -30,6 +34,67 @@ class EnterTrainingTeamRepo(CustomRecognition):
         | tuple[int, int, int, int]
         | None
     ):
+        # 识别关卡是否还有剩余次数
+        r = context.run_recognition_direct(
+            JRecognitionType.TemplateMatch,
+            JTemplateMatch(["Battle/BattleTraining/11.png"], roi=(192, 524, 900, 68)),
+            argv.image,
+        )
+        if r is None or not r.hit:
+            return
+
+        # 识别关卡名字
+        r2 = context.run_recognition_direct(
+            JRecognitionType.OCR,
+            JOCR(
+                ["战斗演习", "狩猎行动", "拓展训练", "资源筹备"],
+                roi=toTuple(r.box),
+                roi_offset=(-63, -243, 126, 42),
+            ),
+            argv.image,
+        )
+        if r2 is None or not r2.hit:
+            return
+        b_r2 = r2.best_result
+        assert type(b_r2) == OCRResult
+
+        # 覆写关卡信息
+        name = b_r2.text
+        attach: dict[str, str] = (context.get_node_data("TrainingEnterOne") or {}).get(
+            "attach", {}
+        )
+        context.override_pipeline(
+            {
+                "TrainingEnterOne": {"focus": {"Node.Action.Starting": f"选择{name}"}},
+                "TrainingEnterFormation": {
+                    "custom_recognition_param": attach[name],
+                    "focus": {"Node.Action.Starting": f"选择难度{attach[name]}"},
+                },
+            }
+        )
+
+        # 返回文字位置
+        return r2.box
+
+
+# 识别关卡难度 返回匹配难度的出击按钮box
+@AgentServer.custom_recognition("TrainingEnterFormationRepo")
+class TrainingEnterFormationRepo(CustomRecognition):
+    def analyze(
+        self, context: Context, argv: CustomRecognition.AnalyzeArg
+    ) -> (
+        CustomRecognition.AnalyzeResult
+        | Rect
+        | list[int]
+        | ndarray[tuple[Any, ...], dtype[Any]]
+        | tuple[int, int, int, int]
+        | None
+    ):
+        # 提取变量
+        s = argv.custom_recognition_param
+        level: str = json.loads(s) if s is not None else "六"
+
+        # 识别难度
         result = context.run_recognition_direct(
             JRecognitionType.OCR,
             JOCR([r".{0,}难度.+"], roi=(470, 125, 158, 434)),
@@ -37,11 +102,9 @@ class EnterTrainingTeamRepo(CustomRecognition):
         )
         if not result or not result.hit:
             return
-        l = result.filtered_results
-        for i in l:
+
+        # 遍历判断关卡难度
+        for i in result.filtered_results:
             assert type(i) == OCRResult
-            m = search(r"难度(.)", i.text)
-            if not m:
-                continue
-            if m.group(1) == "六":
+            if level in i.text:
                 return (960, i.box[1], 93, 97)
